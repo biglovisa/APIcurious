c47ab04f165f15444b77f70497692543ab03a7ad
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-08 14:55:51 -0600

    add gems, remove comments and update spec'

--- spec/features/guest_visits_root_spec.rb
+++ spec/features/guest_visits_root_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe "Guest visits root page", type: :feature do
 
       expect(current_path).to eq root_path
 
-      within(".header") do
-        expect(page).to have_content "Otherhub"
-      end
-
       within(".main") do
         expect(page).to have_content "Otherhub"
         expect(page).to have_link "Sign in with GitHub"
+      end
+
+      within(".contact") do
+        expect(page).to have_link "Repository"
+        expect(page).to have_link "Contact"
       end
     end
   end
