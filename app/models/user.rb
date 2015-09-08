5047098c79534a0d082ba4037c647b59bcb792cf
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-08 15:45:09 -0600

    add user model, create User from auth data

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+
+    user.email = data.info.email
+    user.nickname = data.info.nickname
+    user.image_url = data.info.image
+    user.token = data.info.image
+    user.save
+
+    user
+  end
+end
+
