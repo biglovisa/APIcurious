77c9be285c36feaf0e5916ee14ed35afd055ed3d
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-08 16:48:19 -0600

    update user model, add user ctrler, add users#show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   def self.find_or_create_from_auth(data)
-    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+    user = User.find_or_create_by(provider: data['provider'], uid: data['uid'])
 
-    user.email = data.info.email
-    user.nickname = data.info.nickname
-    user.image_url = data.info.image
-    user.token = data.info.image
+    user.email = data['info']['email']
+    user.nickname = data['info']['nickname']
+    user.image_url = data['info']['image']
+    user.token = data['info']['token']
     user.save
 
     user
