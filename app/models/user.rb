dad8ba7e1e669257a83607df3bb72e629a3d4d26
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-09 16:26:53 -0600

    add methods and spec connecting the service with the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,41 @@
 class User < ActiveRecord::Base
   def self.find_or_create_from_auth(data)
-    user = User.find_or_create_by(provider: data['provider'], uid: data['uid'])
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
 
-    user.email = data['info']['email']
-    user.nickname = data['info']['nickname']
-    user.image_url = data['info']['image']
-    user.token = data['info']['token']
+    user.email = data.info.email,
+    user.nickname = data.info.nickname,
+    user.image_url = data.info.image,
+    user.token = data.info.token
     user.save
 
     user
   end
 
   def self.service
-    @service = GithubService.new
+    @service ||= GithubService.new
   end
 
-  # get all the users followers
-  # get all the users the user is following
-  # get all the users commits
-  # get all users repos
-  # get all the users organizations
-  # get all the users
-end
+  def service
+    self.class.service
+  end
 
+  def repos(user)
+    service.find_user_repos(user)
+  end
+
+  def starred_repos(user)
+    service.find_starred_repos(user)
+  end
+
+  def followers(user)
+    service.find_followers(user)
+  end
+
+  def following(user)
+    service.find_following(user)
+  end
+
+  def organizations(user)
+    service.find_user_organizations(user)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,4 +30,41 @@ RSpec.describe "the User model", type: :model do
       expect(user.token).to eq "abcdef"
     end
   end
+
+  context "a valid user's GitHub data" do
+    it "returns the user's repos" do
+      VCR.use_cassette("user_repos") do
+        repos = user.repos(user)
+        expect(repos.count).to eq 3
+      end
+    end
+
+    it "returns a user's starred repos" do
+      VCR.use_cassette("starred") do
+        starred = user.starred_repos(user)
+        expect(starred.count).to eq 30
+      end
+    end
+
+    it "returns a user's followers" do
+      VCR.use_cassette("user_followers") do
+        followers = user.followers(user)
+        expect(followers.count).to eq 6
+      end
+    end
+
+    it "returns the users the user is following" do
+      VCR.use_cassette("following") do
+        following = user.following(user)
+        expect(following.count).to eq 2
+      end
+    end
+
+    it "returns the organizations the user is a part of" do
+      VCR.use_cassette("user_organizations") do
+        organizations = user.organizations(user)
+        expect(organizations).to eq []
+      end
+    end
+  end
 end
