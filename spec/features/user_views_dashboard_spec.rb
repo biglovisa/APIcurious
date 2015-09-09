6722dfaa4044e45d5cfd05b1da3b9908124c260e
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-08 23:04:21 -0600

    header spec passing

---
+++ spec/features/user_views_dashboard_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "User visits the dashboard" do
+  context "a logged in user" do
+    before(:each) do
+      login_user
+      visit root_path
+      click_link "Sign in with GitHub"
+    end
+
+    it "views their nickname in the header" do
+      expect(current_path).to eq dashboard_path
+
+      within(".dashboard-main") do
+        expect(page).to have_content user.nickname
+        expect(page).to have_link "latest commit"
+      end
+    end
+  end
+end
