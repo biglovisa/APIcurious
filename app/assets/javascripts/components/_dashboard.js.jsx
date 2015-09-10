fcd18c67b518dd9da6d6de3dfa280016c0c73237
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-10 00:06:31 -0600

    add button row

--- app/assets/javascripts/components/_dashboard.js.jsx
+++ app/assets/javascripts/components/_dashboard.js.jsx
@@ -3,8 +3,6 @@ var Dashboard = React.createClass ({
     return { data: {} }
   },
   render: function() {
-
-
     return (
       <StatsBar following={ this.props.data.following } followers={ this.props.data.followers } starredRepos={ this.props.data.starredRepos }  />
     );
@@ -12,32 +10,40 @@ var Dashboard = React.createClass ({
 });
 
 var StatsBar = React.createClass ({
-  handleButtonClick: function(button) {
-    console.log(button);
+  getInitialState: function() {
+    return { activeButton: 'starred' };
+  },
+  handleButtonClick: function(clicked) {
+    this.setState({activeButton: clicked});
   },
   render: function() {
     return (
-      <div className="btn-group btn-group-justified stats-bar" role="group" aria-label="...">
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "starredRepos") }>Starred Repos  <span className="glyphicon glyphicon-star"></span></button>
+      <div className="top-column">
+        <div className="btn-group btn-group-justified stats-bar" role="group" aria-label="...">
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "starredRepos") }>Starred Repos  <span className="glyphicon glyphicon-star"></span></button>
+          </div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "followers") }>Followers</button>
+          </div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "following") }>Following</button>
+          </div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "repositories") }>Repositories</button>
+          </div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "commits") }>Commit History</button>
+          </div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "organizations") }>Organizations</button>
+          </div>
         </div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "followers") }>Followers</button>
-        </div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "following") }>Following</button>
-        </div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "repositories") }>Repositories</button>
-        </div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "commits") }>Commit History</button>
-        </div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary" onClick={ this.handleButtonClick.bind(this, "organizations") }>Organizations</button>
+
+        <div className="content">
+          <StarredRepos starredRepos={this.props.starredRepos} />
         </div>
       </div>
     );
   }
 });
-
