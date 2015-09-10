0bf0a08605adbd45876a52a59c6cf2458b3eee7c
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-10 15:35:23 -0600

    cassettes and spec helper update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ VCR.configure do |c|
 end
 
 RSpec.configure do |config|
-  stub_request(:any, "www.localhost:9292")
+  WebMock.stub_request(:any, "www.localhost:9292")
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
