93f9b99507f647057a08bf4d414205b883036f4a
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-09 22:21:16 -0600

    render react component in view, wire data to component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   before_action :authorize!
 
+
   def show
+    @user ||= current_user
+    @following ||= current_user.following
+    @followers ||= current_user.followers
+    @starred_repos ||= current_user.starred_repos
+
+    @data = {
+      user: @user,
+      following: @following,
+      followers: @followers,
+      starredRepos: @starred_repos
+    }
   end
 end
