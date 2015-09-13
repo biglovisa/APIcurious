c2d6b4134c7261012f8be16df0f5c673a8da6c53
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-13 08:18:47 -0600

    trying to hide the giant data hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,7 @@
 class UsersController < ApplicationController
   before_action :authorize!
 
-
   def show
-    @user            ||= current_user
-    @following       ||= current_user.following
-    @followers       ||= current_user.followers
-    @starred_repos   ||= current_user.starred_repos
-    @organizations   ||= current_user.organizations
-    @repositories    ||= current_user.repos
-    @current_streak  ||= current_user.current_streak
-    @longest_streak  ||= current_user.longest_streak
-    @daily_commits   ||= current_user.daily_commits
-    @total_commits   ||= current_user.total_commits
-    @received_events ||= current_user.recent_events
-
-    @data = {
-      user:             @user,
-      users:            { following: @following,
-                          followers: @followers },
-      starredRepos:     @starred_repos,
-      organizations:    @organizations,
-      repositories:     @repositories,
-      commit_history:   { current_streak: @current_streak,
-                          longest_streak: @longest_streak,
-                          daily_commits: @daily_commits,
-                          total_commits: @total_commits },
-      received_events:  @received_events
-    }
+    @data = Builder.data(current_user)
   end
 end
---
+++ app/models/builder.rb
@@ -0,0 +1,29 @@
+class Builder
+  def self.data(user)
+    @user            ||= user
+    @following       ||= user.following
+    @followers       ||= user.followers
+    @starred_repos   ||= user.starred_repos
+    @organizations   ||= user.organizations
+    @repositories    ||= user.repos
+    @current_streak  ||= user.current_streak
+    @longest_streak  ||= user.longest_streak
+    @daily_commits   ||= user.daily_commits
+    @total_commits   ||= user.total_commits
+    @received_events ||= user.recent_events
+
+    @data = {
+      user:             @user,
+      users:            { following: @following,
+                          followers: @followers },
+      starredRepos:     @starred_repos,
+      organizations:    @organizations,
+      repositories:     @repositories,
+      commit_history:   { current_streak: @current_streak,
+                          longest_streak: @longest_streak,
+                          daily_commits: @daily_commits,
+                          total_commits: @total_commits },
+      received_events:  @received_events
+    }
+  end
+end
