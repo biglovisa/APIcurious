80aef9d6a04949b7a54e3a184de0396c6e278844
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-15 12:59:13 -0600

    change incoming data to be a state, not a prop

--- app/assets/javascripts/components/_dashboard.js.jsx
+++ app/assets/javascripts/components/_dashboard.js.jsx
@@ -1,32 +1,36 @@
 // TODO:
   // on click "follow" or "unfollow", refresh the list of following
-  // on click "unfollow", make it possible to re follow
-  // on click "follow", make it possible to unfollow
+  // on click "unfollow", user can re follow
+  // on click "follow", user can unfollow
   // on search, users that are already followed, show the "unfollow" button
   // build out the feed more, add recent commits
   // show list of user's recent commit messages and repos
   // repos bubble chart
 
 var Dashboard = React.createClass ({
-  getDefaultProps: function() {
-    return { data: {} }
+  getInitialState: function() {
+    data = window.data
+    data.following = data.users.following
+    data.followers = data.users.followers
+
+    return data
   },
   render: function() {
     return (
       <div>
         <div className='col-lg-6'>
           <StatsBar key='statsbar'
-                    starredRepos= { this.props.data.starredRepos }
-                    organizations={ this.props.data.organizations }
-                    repositories= { this.props.data.repositories }
-                    commitHistory={ this.props.data.commit_history } />
+                    starredRepos= { this.state.starredRepos }
+                    organizations={ this.state.organizations }
+                    repositories= { this.state.repositories }
+                    commitHistory={ this.state.commit_history } />
         </div>
         <div className='col-lg-6'>
           <UsersBar key='usersbar'
-                    following=  { this.props.data.users.following }
-                    followers=  { this.props.data.users.followers }
-                    feed=       { this.props.data.received_events }
-                    currentUser={ this.props.data.user } />
+                    following=  { this.state.following }
+                    followers=  { this.state.followers }
+                    feed=       { this.state.received_events }
+                    currentUser={ this.state.user } />
         </div>
       </div>
     );
